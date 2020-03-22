8a6015e24ae4ce7707be5c40be908cb80ee01ac2
wenshuo <1443678932@qq.com>
2020-03-22 11:42:09 +0800

    Initial Commit

---
+++ SimpleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleApp
+//
+//  Created by Wenshuo on 2020/3/22.
+//  Copyright © 2020年 Wenshuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleApp
+//
+//  Created by Wenshuo on 2020/3/22.
+//  Copyright © 2020年 Wenshuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
